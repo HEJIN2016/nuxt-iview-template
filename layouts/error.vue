5111cf15bdf17e328c62a6fc2398bf1a972d49ab
hejin <1179767901@qq.com>
2019-08-02 14:24:07 +0800

    add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ error }}</div>
+</template>
+
+<script>
+  export default {
+    name: "error",
+    props: {
+      error: {
+        type: Object,
+        default () {
+          return {}
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
